17e14db79b0bfec078ef5cfb4a0a83ee9f22f6d1
Luiz Lopes <luizlopesdev@gmail.com>
2020-03-07 11:51:27 -0300

    Add fizzbuzz example

--- src/value.rs
+++ src/value.rs
@@ -2,27 +2,40 @@ use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 pub enum Value {
-    String(String),
-    Number(f64),
-    Boolean(bool),
-    Object(HashMap<String, Value>),
+    String {
+        value: String
+    },
+    Integer {
+        value: i64
+    },
+    Decimal {
+        value: f64
+    },
+    Array {
+        value: Vec<Value>
+    },
+    Object {
+        value: HashMap<String, Value>
+    },
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum ValueKind {
     String,
-    Number,
-    Boolean,
+    Decimal,
+    Integer,
+    Array,
     Object,
 }
 
 impl Value {
     pub fn is(&self, kind: ValueKind) -> bool {
         match self {
-            Value::String(_)  => kind == ValueKind::String,
-            Value::Number(_)  => kind == ValueKind::Number,
-            Value::Boolean(_) => kind == ValueKind::Boolean,
-            Value::Object(_)  => kind == ValueKind::Object,
+            Value::String {..} => kind == ValueKind::String,
+            Value::Decimal{..} => kind == ValueKind::Decimal,
+            Value::Integer{..} => kind == ValueKind::Integer,
+            Value::Array  {..} => kind == ValueKind::Array,
+            Value::Object {..} => kind == ValueKind::Object,
         }
     }
 }
